639e7e11c816d4218ee97f27a0e1e16367398c63
sgm17 <sergigm78@gmail.com>
2024-06-04 18:24:37 +0200

    conversations include messages

--- pages/api/v1/conversations.ts
+++ pages/api/v1/conversations.ts
@@ -186,6 +186,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                                 }
                             }
                         },
+                        messages: true
                     }
                 })
                 res.status(200).json(conversations)
